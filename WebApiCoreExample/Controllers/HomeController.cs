c1bd86f93850f6f691587da89100504b54b8aa8e
Jean Pierre DeffoFotso <defcoq73@yahoo.fr>
2019-04-06 23:41:12 +0200

    Add RestFull Project

---
+++ WebApiCoreExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCoreExample.Models;
+
+namespace WebApiCoreExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository { get; set; }
+
+        public HomeController(IRepository repo) => repository = repo;
+
+        public ViewResult Index() => View(repository.Reservations);
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
